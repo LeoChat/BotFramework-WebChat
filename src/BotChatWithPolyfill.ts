5ec63f5ccdf4b4c01d6189b02a83735febb87951
Guy Ambar <guy@meetleo.co>
2018-11-08 17:41:26 +0200

    added import array includes polyfill

--- src/BotChatWithPolyfill.ts
+++ src/BotChatWithPolyfill.ts
@@ -3,7 +3,9 @@ export * from './BotChat';
 // below are shims for compatibility with old browsers (IE 10 being the main culprit)
 import 'core-js/modules/es6.array.find';
 import 'core-js/modules/es6.array.find-index';
+import 'core-js/modules/es6.string.includes';
 import 'core-js/modules/es6.string.starts-with';
+import 'core-js/modules/es7.array.includes';
 
 import * as BluebirdPromise from 'bluebird';
 
